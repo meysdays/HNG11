9fc7dd5aa1c72109923ee59749f70b1e97d4766b
Igbokwe Victor <igbokwevictor122@gmail.com>
2024-07-01 00:46:34 +0100

    get the current temperature using weatherapi

--- restapi/restapi/src/main/java/com/meysdays/restapi/service/GetInfoService.java
+++ restapi/restapi/src/main/java/com/meysdays/restapi/service/GetInfoService.java
@@ -14,6 +14,9 @@ public class GetInfoService {
 
     private final String API_URL = "http://ipinfo.io/{ip}/geo?token={token}";
     private final String API_TOKEN = "9c8367e0d40335";
+
+    private final String API_URL2 = "http://api.weatherapi.com/v1/current.json?key=358f90de982a48f69d0184008231605&q={city}&aqi=no";
+    private final String API_KEY = "358f90de982a48f69d0184008231605";
     public Response getInfoService(HttpServletRequest request, String name){
         String clientIp = request.getHeader("X-Forwarded-For");
         if (clientIp == null || clientIp.isEmpty()) {
@@ -24,8 +27,15 @@ public class GetInfoService {
             clientIp = clientIp.split(",")[0];
         }
         String city = getLocation(clientIp, API_TOKEN);
-        
-        return new Response(clientIp, name, city != null ? city : "City not found");
+        String c = city != null ? city : "City not found";
+
+        Double temperature = getTemperature(c);
+        Map<String, Object> response = new HashMap<>();
+        response.put("temperature", temperature);
+
+        String f = "Hello, "+name+"!, the temperature is "+temperature+" degrees Celcius in "+c;
+
+        return new Response(clientIp, name, f);
     }
 
     public String getLocation(String ip, String token){
@@ -36,5 +46,38 @@ public class GetInfoService {
 
         Map<String, Object> response = restTemplate.getForObject(API_URL,Map.class, uriVariables);
         return response != null ? (String) response.get("city") : null;
+    }
+
+    public Map<String, Object> getWeather(String city){
+        RestTemplate restTemplate = new RestTemplate();
+        Map<String, Object> response = restTemplate.getForObject(API_URL2, Map.class, city);
+
+        Map<String, Object> result = new HashMap<>();
+        if (response != null) {
+            //Map<String, Object> location = (Map<String, Object>) response.get("location");
+            Map<String, Object> current = (Map<String, Object>) response.get("current");
+
+//            if (location != null) {
+//                result.put("cityName", location.get("name"));
+//                result.put("region", location.get("region"));
+//                result.put("country", location.get("country"));
+//                result.put("latitude", location.get("lat"));
+//                result.put("longitude", location.get("lon"));
+//                result.put("timezone", location.get("tz_id"));
+//                result.put("localtime", location.get("localtime"));
+//            }
+
+            if (current != null) {
+                result.put("temperature", current.get("temp_c"));
+                result.put("humidity", current.get("humidity"));
+            }
+        }
+
+        return result;
+    }
+
+    public Double getTemperature(String city) {
+        Map<String, Object> weatherData = getWeather(city);
+        return weatherData.containsKey("temperature") ? (Double) weatherData.get("temperature") : null;
     }
 }
